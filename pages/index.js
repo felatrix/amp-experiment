6abc4607f501096c7ae0c4e614cea7cef0ed87a1
Iwan Darmawan <iwan.darmawan@orami.com>
2022-01-18 14:43:11 +0700

    amp initial

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { useAmp } from 'next/amp';
+
+export const config = { amp: 'hybrid' };
 
 export default function Home() {
   return (
@@ -8,13 +11,38 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
+        <script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
       </Head>
-
+      <amp-analytics config="https://www.googletagmanager.com/amp.json?id=GTM-NR8C452&gtm.url=SOURCE_URL" data-credentials="include"></amp-analytics>
+      <amp-analytics id="amp-gtm-googleanalytics" type="googleanalytics" data-credentials="include">
+          <script
+            type="application/json"
+            dangerouslySetInnerHTML={{
+              __html: `{
+                "vars": {
+                  "account":"UA-151629277-1"
+                },
+                "triggers": {
+                  "button": {
+                    "selector": "#the-button",
+                    "on": "click",
+                    "vars": {
+                      "event_name": "login",
+                      "method": "Google"
+                    }
+                  }
+                }
+              }`
+            }}
+          />
+        </amp-analytics>
       <main>
         <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        {  useAmp() ? (
+          <p id="the-button">This is the <strong>AMP</strong> version of the home page</p>
+        ) : (
+          <p>This is the main version of the home page</p>
+        )}
       </main>
 
       <Footer />
